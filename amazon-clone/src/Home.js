52e9a66c510891713b130df2f11f4b90aebcb529
shalimar mehra <shalimarmehra1010@gmail.com>
2023-09-18 19:38:14 +0530

    forth commit

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -13,23 +13,47 @@ function Home() {
       />
       <div className="home__row">
         <Product
-        // title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-        // price={989.99}
-        // image="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-        // rating={5}
+          title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+          price={989.99}
+          image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          rating={5}
         />
-        <Product />
+        <Product
+          title="Sandisk Cruzer Blade CZ50 USB 2.0 Pendrive (16 GB) - Pack of 5"
+          price={999}
+          image="https://images-na.ssl-images-amazon.com/images/I/41r9HAJFJaL.jpg"
+          rating={4}
+        />
       </div>
 
       <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          title="Pigeon by Stovekraft Kessel 1.2-Litre Multi-Purpose Kettle (Silver) 600W, Black"
+          price={799}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Y21By5TzL._SL1000_.jpg"
+          rating={3}
+        />
+        <Product
+          title="Mediweave KN95 (Equivalent to N95) Mask/Respirator,CE certified, Pack of 5"
+          price={199.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/71grDnU5aQL._SL1500_.jpg"
+          rating={5}
+        />
+        <Product
+          title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+          price={59811.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          rating={5}
+        />
       </div>
 
       <div className="home__row">
-        <Product />
-        <Product />
+        <Product
+          title="OPPO F19 Pro (Crystal Silver, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers"
+          price={59844.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/71KlQWpei4L._SL1500_.jpg"
+          rating={5}
+        />
       </div>
     </div>
   );
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
-          <small>$</small>
-          <strong>19.99</strong>
+          <small>₹</small>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
 
-      <img
-        src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-        alt=""
-      />
+      <img src={image} alt="" />
 
       <button>Add to Basket</button>
     </div>
